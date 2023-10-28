7a007b7b7e0ba774e3abf7f78e84dafc81b27ea6
vmC0sta <duda.cv2404@gmail.com>
2023-10-28 18:23:43 -0300

    feat
    Após clicar no button do content__initial irá abrir um novo elemento content__sucess

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const labelEmail = document.getElementById('validation')
 const inputEmail = document.getElementById('email');
 const btnEnviar = document.getElementById('btn-enviar');
+const emailSucess = document.getElementById('email-sucess')
+const contentInitial = document.getElementsByClassName('content__initial')[0]
+const contentSucess = document.getElementsByClassName('content__sucess')[0]
 
-inputEmail.addEventListener('keydown', () => {
+inputEmail.addEventListener('keyup', () => {
 
     if (!validarEmail(inputEmail.value)) {
         inputEmail.classList.remove('accepted-validation-input')
@@ -19,7 +22,6 @@ inputEmail.addEventListener('keydown', () => {
 
         labelEmail.classList.remove('no-accepted-validation-label')
         labelEmail.classList.add('accepted-validation-label')
-        
         labelEmail.innerHTML = 'Perfect!'
     }
 })
@@ -28,3 +30,14 @@ inputEmail.addEventListener('keydown', () => {
 function validarEmail(email) {
     return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
 }
+
+btnEnviar.addEventListener('click',()=>{
+
+    if(validarEmail(inputEmail.value)){
+        emailSucess.innerHTML = inputEmail.value
+        contentInitial.style.display = 'none'
+        contentSucess.style.display = 'block'
+
+    }
+
+})
